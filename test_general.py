dc642c21857cfc768948456dfcd7f5f8c2636755
MacFan4000 <paulfkeffer@gmail.com>
2021-02-16 11:28:41 -0500

    fix

--- test_general.py
+++ test_general.py
@@ -18,7 +18,7 @@ def test_line_length():
             with open(os.path.join(PLUGINPATH, filen)) as python_source:
                 src = python_source.readlines()
                 for line_number, line in enumerate(src):
-                    assert len(line.strip()) < MAX_LENGTH, 'length of line #{0} exceeds limit'.format(line_number)
+                    assert len(line.strip()) < MAX_LENGTH, f'length of line #{line_number} exceeds limit'
 
 
 def test_no_get_on_lists():
